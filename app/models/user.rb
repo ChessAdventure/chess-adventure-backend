9abbfb78fdf38c150a3106bbee7eb81b5bed9d40
Max Ribbans <ribbans.max@gmail.com>
2021-04-08 21:25:43 -0400

    Add validations to user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true
-  validates :username, uniqueness: true
+  validates :username, uniqueness: { case_sensitive: false }
   validates :username, length: { minimum: 4 }
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 end
