1af28ebeea7694a79b6522bdd50ccda377cdff46
Max Ribbans <ribbans.max@gmail.com>
2021-04-08 20:48:50 -0400

    User routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/users',         to: 'users#create'
+  get '/users/:user_id', to: 'users#show'
+  get '/users',          to: 'users#index'
 end
