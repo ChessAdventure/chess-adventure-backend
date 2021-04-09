65db70b72e6b1c0b8f576679aea6acfa8f108a6a
Max Ribbans <ribbans.max@gmail.com>
2021-04-08 21:14:06 -0400

    Add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   post '/users',         to: 'users#create'
   get '/users/:user_id', to: 'users#show'
   get '/users',          to: 'users#index'
+  post '/login',         to: 'sessions#create'
+  post '/logout',        to: 'sessions#destroy'
+  get '/logged_in',      to: 'sessions#is_logged_in?'
 end
