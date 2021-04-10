23dbe395a0ac9d78fa18fbeffea7a6d431b1e357
Max Ribbans <ribbans.max@gmail.com>
2021-04-10 12:08:14 -0400

    Remove email from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: { case_sensitive: false }
   validates :username, length: { minimum: 4 }
-  validates :email, presence: true
-  validates :email, uniqueness: { case_sensitive: false }
 end
