09e73c0ffdba199e0829b998b880bdf19eb3c278
Max Ribbans <ribbans.max@gmail.com>
2021-04-12 15:56:04 -0400

    Add friendly_games_model

---
+++ spec/models/friendly_game_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe FriendlyGame, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :status }
+    it { should validate_presence_of(:current_fen).on(:update) }
+    it { should validate_presence_of :starting_fen }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :white }
+    it { should belong_to :black }
+  end
+end
