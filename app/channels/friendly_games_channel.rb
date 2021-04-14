27d159d130664542ee605a4c040ec1a7e85259a0
Max Ribbans <ribbans.max@gmail.com>
2021-04-14 19:04:42 -0400

    Add comments for FE development

--- app/channels/friendly_games_channel.rb
+++ app/channels/friendly_games_channel.rb
@@ -1,14 +1,16 @@
 class FriendlyGamesChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "friendly_games_channel"
-    FriendlyGame.create!(starting_fen: 'a')
+    stream_from "friendly_games_channel" # when ready, change this to:
+    #  stream_from "friendly_games_channel_#{friendly_game.extension}"
+    # this is where you should call params
+    binding.pry
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def game_move(fen, key)
-
-  end
+  # def game_move(fen, key)
+  #   some other method that you can call on the front end?
+  # end
 end
--- app/controllers/api/v1/friendly_games_controller.rb
+++ app/controllers/api/v1/friendly_games_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FriendlyGamesController < ApplicationController
-  def create
-    binding.pry
-    ActionCable.server.broadcast 'friendly_games_channel', {"hello": "hi bye"}
-  end
+  # def create
+  #   binding.pry
+  #   ActionCable.server.broadcast 'friendly_games_channel', {"hello": "hi bye"}
+  # end
 end
