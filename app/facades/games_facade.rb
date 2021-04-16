3aea1df5d2d976d7a28b8e58129e05ddd65651ed
Max Ribbans <ribbans.max@gmail.com>
2021-04-15 22:25:15 -0400

    Add add_player? method to update game if it needs a player, and that player is not already playing

--- app/facades/games_facade.rb
+++ app/facades/games_facade.rb
@@ -9,7 +9,25 @@ class GamesFacade
         FriendlyGame.create(starting_fen: fen.fen)
       end
     end
-  
+
+    def add_player?(ext, api)
+      # TODO fix this method to not clog the server if 1000 people subscribe
+      # likely there should be a guard clause in the channel, not just here
+      # maybe two clauses, one for ext, one for valid api_key
+      game = FriendlyGame.find_by(extension: ext) rescue nil
+      id = User.find_by(api_key: api).id rescue nil
+      if game and (game.black_id || ((game.white_id != nil) && game.white_id == id))
+        true
+      else
+        if game.white_id
+          game.black_id = id
+        else
+          game.white_id = id
+        end
+        game.save
+      end
+    end
+
     private
   end
 end
