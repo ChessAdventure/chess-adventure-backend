7dbcbd73fb1397f9e7ef76e57abc7b67f63af2a5
Max Ribbans <ribbans.max@gmail.com>
2021-04-16 11:58:35 -0400

    Change subscription to provide game's fen on subscription
    Co-authored-by: Chris Spohn <69563078+CJSpohn@users.noreply.github.com>
    Co-authored-by: Elsa Fluss <elsafluss@users.noreply.github.com>

--- app/channels/friendly_games_channel.rb
+++ app/channels/friendly_games_channel.rb
@@ -2,6 +2,7 @@ class FriendlyGamesChannel < ApplicationCable::Channel
   def subscribed
     stream_from "friendly_games_channel_#{params[:extension]}"
     GamesFacade.add_player?(params[:extension], params[:api_key])
+    ActionCable.server.broadcast "friendly_games_channel_#{params[:extension]}", FriendlyGameSerializer.new(FriendlyGame.find_by(extension: params[:extension]))
   end
 
   def unsubscribed
