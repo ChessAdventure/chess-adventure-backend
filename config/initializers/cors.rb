662491f2b318498a3e3f1629f97cb5d0085bbcae
Max Ribbans <ribbans.max@gmail.com>
2021-04-16 17:53:43 -0400

    origins allowed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://chessquestadventuretest.herokuapp.com'
+    origins [/http:\/\/*/, /https:\/\/*/] # 'https://chessquestadventuretest.herokuapp.com'
 
     resource '*',
       headers: :any,
