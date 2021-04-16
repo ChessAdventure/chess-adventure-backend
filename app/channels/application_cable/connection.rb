5d8cce0c45357327a51175d2691ded13ac446a3d
Max Ribbans <ribbans.max@gmail.com>
2021-04-15 22:27:09 -0400

    Remove unused code

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,15 +1,4 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :user_id
-
-    def connect
-      self.user_id = find_id
-    end
-
-    private
-
-    def find_id
-      binding.pry
-    end
   end
 end
