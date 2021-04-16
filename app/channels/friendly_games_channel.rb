a23d3cc88be33a49daa30e70b3ec0af735ea7ab7
Max Ribbans <ribbans.max@gmail.com>
2021-04-15 22:27:36 -0400

    Call add_player upon subscription

--- app/channels/friendly_games_channel.rb
+++ app/channels/friendly_games_channel.rb
@@ -1,16 +1,10 @@
 class FriendlyGamesChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "friendly_games_channel" # when ready, change this to:
-    #  stream_from "friendly_games_channel_#{friendly_game.extension}"
-    # this is where you should call params
-    # binding.pry
+    stream_from "friendly_games_channel_#{params[:extension]}"
+    GamesFacade.add_player?(params[:extension], params[:api_key])
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
-
-  # def game_move(fen, key)
-  #   some other method that you can call on the front end?
-  # end
 end
