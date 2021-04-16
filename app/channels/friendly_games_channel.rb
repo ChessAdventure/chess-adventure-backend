3c0ddedd5911631b2d7900d85d9de3fcf99b1a8f
Max Ribbans <ribbans.max@gmail.com>
2021-04-16 13:52:08 -0400

    render > broadcast in channels

--- app/channels/friendly_games_channel.rb
+++ app/channels/friendly_games_channel.rb
@@ -2,7 +2,7 @@ class FriendlyGamesChannel < ApplicationCable::Channel
   def subscribed
     stream_from "friendly_games_channel_#{params[:extension]}"
     GamesFacade.add_player?(params[:extension], params[:api_key])
-    ActionCable.server.broadcast "friendly_games_channel_#{params[:extension]}", FriendlyGameSerializer.new(FriendlyGame.find_by(extension: params[:extension]))
+    render json: FriendlyGameSerializer.new(FriendlyGame.find_by(extension: params[:extension]))
   end
 
   def unsubscribed
