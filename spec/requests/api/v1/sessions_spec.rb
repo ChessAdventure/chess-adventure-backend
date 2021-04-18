a13d9fdb69bcd971185100d36b2f43473e54c144
mainlyetcetera <ericcampbell294@gmail.com>
2021-04-18 12:03:05 -0600

    Adjust describes, add test, can check logged in
    Co-authored-by: V Arruda <69529052+nessaarruda@users.noreply.github.com>
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -1,118 +1,147 @@
 require 'rails_helper'
 
 describe 'User paths' do
-  describe 'happy' do
-    it 'should be able to login' do
-      user = User.create(username: 'John Doe', password: 'Password')
+  describe 'logging in' do 
+    describe 'happy' do
+      it 'should be able to login' do
+        user = User.create(username: 'John Doe', password: 'Password')
 
-      data = {
-        user: {
-          username: 'John Doe',
-          password: 'Password',
-          password_confirmation: 'Password'
+        data = {
+          user: {
+            username: 'John Doe',
+            password: 'Password',
+            password_confirmation: 'Password'
+          }
         }
-      }
-      headers = { 'CONTENT_TYPE' => 'application/json' }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
 
-      post '/api/v1/login', params: JSON.generate(data), headers: headers
+        post '/api/v1/login', params: JSON.generate(data), headers: headers
 
-      expect(response.status).to eq(200)
+        expect(response.status).to eq(200)
 
-      data = JSON.parse(response.body, symbolize_names: true)[:data]
+        data = JSON.parse(response.body, symbolize_names: true)[:data]
 
-      expect(User.last.api_key).to eq(data[:attributes][:api_key])
-      expect(data[:attributes][:username]).to eq(user.username)
+        expect(User.last.api_key).to eq(data[:attributes][:api_key])
+        expect(data[:attributes][:username]).to eq(user.username)
+      end
+    end
+
+    describe 'sad' do
+      it 'should not find user not there' do
+        user = User.create(username: 'John Doe', password: 'Password')
+
+        data = {
+          user: {
+            username: '',
+            password: '',
+            password_confirmation: ''
+          }
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/login', params: JSON.generate(data), headers: headers
+
+        expect(response.status).to eq(401)
+
+        data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(data[:errors][0]).to eq('no such user, please try again')
+      end
+
+      it 'should not login with incorrect password' do
+        user = User.create(username: 'John Doe', password: 'Password')
+
+        data = {
+          user: {
+            username: 'John Doe',
+            password: 'WrongPassword',
+            password_confirmation: 'WrongPassword'
+          }
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/login', params: JSON.generate(data), headers: headers
+
+        expect(response.status).to eq(401)
+
+        data = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+        expect(data).to eq('no such user, please try again')
+      end
+
+      it 'should not login with missing username' do
+        user = User.create(username: 'John Doe', password: 'Password')
+
+        data = {
+          user: {
+            username: '',
+            password: 'Password',
+            password_confirmation: 'Password'
+          }
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/login', params: JSON.generate(data), headers: headers
+
+        expect(response.status).to eq(401)
+
+        data = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+        expect(data).to eq('no such user, please try again')
+      end
+
+      it 'should not login with mismatched password' do
+        user = User.create(username: 'John Doe', password: 'Password', 
+          password_confirmation: 'Some Other Password')
+
+        data = {
+          user: {
+            username: 'John Doe',
+            password: 'Password',
+            password_confirmation: 'Some Other Password'
+          }
+        }
+
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        post '/api/v1/login', params: JSON.generate(data), headers: headers
+
+        expect(response.status).to eq(401)
+
+        data = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+        expect(data).to eq('no such user, please try again')
+      end
     end
   end
 
-  describe 'sad' do
-    it 'should not find user not there' do
-      user = User.create(username: 'John Doe', password: 'Password')
+  describe 'user is logged in method' do
+    describe 'happy' do
+      it 'should be able to login' do
+        user = User.create(username: 'John Doe', password: 'Password')
 
-      data = {
-        user: {
-          username: '',
-          password: '',
-          password_confirmation: ''
+        data = {
+          user: {
+            username: 'John Doe',
+            password: 'Password',
+            password_confirmation: 'Password'
+          }
         }
-      }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
 
-      headers = { 'CONTENT_TYPE' => 'application/json' }
+        post '/api/v1/login', params: JSON.generate(data), headers: headers
 
-      post '/api/v1/login', params: JSON.generate(data), headers: headers
+        get '/api/v1/logged_in', params: JSON.generate(data), headers: headers
 
-      expect(response.status).to eq(401)
+        data = JSON.parse(response.body, symbolize_names: true)
 
-      data = JSON.parse(response.body, symbolize_names: true)
-
-      expect(data[:errors][0]).to eq('no such user, please try again')
-    end
-
-    it 'should not login with incorrect password' do
-      user = User.create(username: 'John Doe', password: 'Password')
-
-      data = {
-        user: {
-          username: 'John Doe',
-          password: 'WrongPassword',
-          password_confirmation: 'WrongPassword'
-        }
-      }
-
-      headers = { 'CONTENT_TYPE' => 'application/json' }
-
-      post '/api/v1/login', params: JSON.generate(data), headers: headers
-
-      expect(response.status).to eq(401)
-
-      data = JSON.parse(response.body, symbolize_names: true)[:errors][0]
-
-      expect(data).to eq('no such user, please try again')
-    end
-
-    it 'should not login with missing username' do
-      user = User.create(username: 'John Doe', password: 'Password')
-
-      data = {
-        user: {
-          username: '',
-          password: 'Password',
-          password_confirmation: 'Password'
-        }
-      }
-
-      headers = { 'CONTENT_TYPE' => 'application/json' }
-
-      post '/api/v1/login', params: JSON.generate(data), headers: headers
-
-      expect(response.status).to eq(401)
-
-      data = JSON.parse(response.body, symbolize_names: true)[:errors][0]
-
-      expect(data).to eq('no such user, please try again')
-    end
-
-    it 'should not login with mismatched password' do
-      user = User.create(username: 'John Doe', password: 'Password', 
-        password_confirmation: 'Some Other Password')
-
-      data = {
-        user: {
-          username: 'John Doe',
-          password: 'Password',
-          password_confirmation: 'Some Other Password'
-        }
-      }
-
-      headers = { 'CONTENT_TYPE' => 'application/json' }
-
-      post '/api/v1/login', params: JSON.generate(data), headers: headers
-
-      expect(response.status).to eq(401)
-
-      data = JSON.parse(response.body, symbolize_names: true)[:errors][0]
-
-      expect(data).to eq('no such user, please try again')
+        expect(response.status).to eq(200)
+        expect(data[:logged_in]).to eq(true)
+        expect(data[:user][:username]).to eq(user.username)
+      end
     end
   end
 
