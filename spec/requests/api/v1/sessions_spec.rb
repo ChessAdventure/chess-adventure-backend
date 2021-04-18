516cea622a12e99954c0da44f366eeb95eca74d4
mainlyetcetera <ericcampbell294@gmail.com>
2021-04-18 11:47:49 -0600

    Add test, cannot login no username
    Co-authored-by: V Arruda <69529052+nessaarruda@users.noreply.github.com>
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -69,6 +69,28 @@ describe 'User paths' do
 
       expect(data).to eq('no such user, please try again')
     end
+
+    it 'should not login with missing username' do
+      user = User.create(username: 'John Doe', password: 'Password')
+
+      data = {
+        user: {
+          username: '',
+          password: 'Password',
+          password_confirmation: 'Password'
+        }
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v1/login', params: JSON.generate(data), headers: headers
+
+      expect(response.status).to eq(401)
+
+      data = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+      expect(data).to eq('no such user, please try again')
+    end
   end
 
 end
