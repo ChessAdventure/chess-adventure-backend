1141024a83658cb2de55dbdaafd2bd687a23bc34
mainlyetcetera <ericcampbell294@gmail.com>
2021-04-19 14:52:49 -0600

    Add test, can log out happy
    Co-authored-by: V Arruda <69529052+nessaarruda@users.noreply.github.com>
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -143,26 +143,60 @@ describe 'User paths' do
         expect(data[:user][:username]).to eq(user.username)
       end
     end
+
+    describe 'sad' do
+      it 'should recognize not logged in' do
+        data = {
+          user: {
+            username: '',
+            password: '',
+            password_confirmation: ''
+          }
+        }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
+
+        get '/api/v1/logged_in', params: JSON.generate(data), headers: headers
+
+        data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(401)
+        expect(data[:logged_in]).to eq(false)
+        expect(data[:message]).to eq('no such user')
+      end
+    end
   end
 
-  describe 'sad' do
-    it 'should recognize not logged in' do
-      data = {
-        user: {
-          username: '',
-          password: '',
-          password_confirmation: ''
+  describe 'logout' do
+    describe 'happy' do
+      it 'a user can log out when logged in' do
+        user = User.create(username: 'John Doe', password: 'Password')
+
+        data = {
+          user: {
+            username: 'John Doe',
+            password: 'Password',
+            password_confirmation: 'Password'
+          }
         }
-      }
-      headers = { 'CONTENT_TYPE' => 'application/json' }
+        headers = { 'CONTENT_TYPE' => 'application/json' }
 
-      get '/api/v1/logged_in', params: JSON.generate(data), headers: headers
+        post '/api/v1/login', params: JSON.generate(data), headers: headers
 
-      data = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(200)
 
-      expect(response.status).to eq(401)
-      expect(data[:logged_in]).to eq(false)
-      expect(data[:message]).to eq('no such user')
+        get '/api/v1/logged_in', params: JSON.generate(data), headers: headers
+
+        expect(response.status).to eq(200)
+
+        data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response.status).to eq(200)
+        expect(data[:logged_in]).to eq(true)
+
+        post '/api/v1/logout', params: JSON.generate(data), headers: headers
+
+        expect(response.status).to eq(200)
+      end
     end
   end
 
