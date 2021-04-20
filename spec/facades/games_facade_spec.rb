b418218f49779f3b2f7833f84abf96e7e376ceb3
Max Ribbans <ribbans.max@gmail.com>
2021-04-19 21:19:23 -0400

    Happy tests for repurpose old game into new game

--- spec/facades/games_facade_spec.rb
+++ spec/facades/games_facade_spec.rb
@@ -27,6 +27,53 @@ describe GamesFacade do
         expect(new_game.white_id).to eq(game.white_id)
         expect(new_game.black_id).to eq(game.black_id)
       end
+
+      it 'should properly keep white as white if it wins' do
+        user = User.create(username: 'John Doe', password: 'Password')
+        user2 = User.create(username: 'Jane Doe', password: 'Password')
+        game = FriendlyGame.create(starting_fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPP2/RNBQKBNR w KQkq - 0 1', current_fen: '1R6/p2r4/2ppkp2/6p1/2PKP2p/P4P2/6PP/8 b - - 0 0', white_id: user.id, black_id: user2.id, status: 1)
+
+        params = {
+          extension: game.extension
+        }
+
+        new_game = GamesFacade.repurpose(params)
+        expect(new_game.current_fen).to eq('rnbqkbnr/pppppppp/8/8/8/8/PPPPPP2/R3K3 w Qkq - 0 1')
+        expect(new_game.white_id).to eq(game.white_id)
+        expect(new_game.black_id).to eq(game.black_id)
+      end
+
+      it 'should properly keep make black white if it wins' do
+        user = User.create(username: 'John Doe', password: 'Password')
+        user2 = User.create(username: 'Jane Doe', password: 'Password')
+        game = FriendlyGame.create(starting_fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPP2/RNBQKBNR w KQkq - 0 1', current_fen: '1R6/3r4/2ppkp2/6p1/2PKP2p/P4P2/6PP/8 b - - 0 0', white_id: user.id, black_id: user2.id, status: 2)
+
+        params = {
+          extension: game.extension
+        }
+
+        new_game = GamesFacade.repurpose(params)
+        expect(new_game.current_fen).to eq('rnbqkbnr/pppppppp/8/8/8/8/PPPPP3/R3K3 w Qkq - 0 1')
+        expect(new_game.white_id).to eq(game.black_id)
+        expect(new_game.black_id).to eq(game.white_id)
+      end
+
+      it 'should not make a new game if one already exists' do
+        user = User.create(username: 'John Doe', password: 'Password')
+        user2 = User.create(username: 'Jane Doe', password: 'Password')
+        game = FriendlyGame.create(starting_fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPP2/RNBQKBNR w KQkq - 0 1', current_fen: '1R6/3r4/2ppkp2/6p1/2PKP2p/P4P2/6PP/8 b - - 0 0', white_id: user.id, black_id: user2.id, status: 2)
+
+        game2 = FriendlyGame.create(starting_fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPP2/RNBQKBNR w KQkq - 0 1')
+
+        game.update(next_game_id: game2.id)
+
+        params = {
+          extension: game.extension
+        }
+
+        new_game = GamesFacade.repurpose(params)
+        expect(new_game).to eq(game2)
+      end
     end
   end
 end
