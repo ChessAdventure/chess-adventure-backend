84f7673ffe8fad9b92761962706a995e40943b24
Max Ribbans <ribbans.max@gmail.com>
2021-05-08 12:56:07 -0400

    Add a

--- app/channels/friendly_games_channel.rb
+++ app/channels/friendly_games_channel.rb
@@ -1,6 +1,7 @@
 class FriendlyGamesChannel < ApplicationCable::Channel
   def subscribed
-    connect(params)
+    a = connect(params)
+    puts a
     GamesFacade.add_player?(params[:extension], current_user.id)
     stream_from "friendly_games_channel_#{params[:extension]}"
     ActionCable.server.broadcast "friendly_games_channel_#{params[:extension]}", FriendlyGameSerializer.new(FriendlyGame.find_by(extension: params[:extension]))
