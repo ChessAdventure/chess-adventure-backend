3c1ae55b6e15e758c6713d4a5883c4033e9de9a7
Max Ribbans <ribbans.max@gmail.com>
2021-05-08 12:03:36 -0400

    Add puts cookies

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,18 +8,7 @@ module ApplicationCable
 
     protected
     def find_verified_user
-      begin
-        header_array = request.headers[:HTTP_SEC_WEBSOCKET_PROTOCOL].split(',')
-        token = header_array[header_array.length-1]
-        decoded_token = JWT.decode token.strip, Rails.application.secrets.secret_key_base, true, { :algorithm => 'HS256' }
-        if (current_user = User.find((decoded_token[0])['sub']))
-          current_user
-        else
-          reject_unauthorized_connection
-        end
-      rescue
-        reject_unauthorized_connection
-      end
+      puts "ENCRYPTED COOKIES: #{cookies.encrypted}"
     end
   end
 end
