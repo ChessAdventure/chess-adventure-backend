82a77d323c624b65746da31c626ceac523bd8373
Max Ribbans <ribbans.max@gmail.com>
2021-05-08 12:53:57 -0400

    Try 6

--- app/channels/friendly_games_channel.rb
+++ app/channels/friendly_games_channel.rb
@@ -1,7 +1,7 @@
 class FriendlyGamesChannel < ApplicationCable::Channel
   def subscribed
-    find_verified_user(params)
-    GamesFacade.add_player?(params[:extension], current_user.id) if :authenticate_user
+    connect(params)
+    GamesFacade.add_player?(params[:extension], current_user.id)
     stream_from "friendly_games_channel_#{params[:extension]}"
     ActionCable.server.broadcast "friendly_games_channel_#{params[:extension]}", FriendlyGameSerializer.new(FriendlyGame.find_by(extension: params[:extension]))
   end
@@ -12,7 +12,16 @@ class FriendlyGamesChannel < ApplicationCable::Channel
 
   protected
 
+  def connect(params)
+    self.current_user = find_verified_user(params)
+  end
+
   def find_verified_user(params)
-    puts "params? #{params[:token]}"
+    token = Knock::AuthToken.new token: params[:token]
+
+    verified_user = User.find_by id: token.payload['sub']
+    return verified_user if verified_user
+
+    reject_unauthorized_connection
   end
 end
