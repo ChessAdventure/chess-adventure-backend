394c185345639faa72939a05ec92ecf0f3eb0ae5
Max Ribbans <ribbans.max@gmail.com>
2021-05-08 16:34:18 -0400

    WIP

--- app/controllers/api/v1/ai_games_controller.rb
+++ app/controllers/api/v1/ai_games_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::FriendlyGamesController < ApplicationController
-  def create
+  before_action :authenticate_user
 
+  def create
+    if current_user.is_fishing?
+      
+    else
+
+    end
   end
 end
