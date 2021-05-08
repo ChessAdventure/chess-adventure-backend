2a8683760482c66da50f588ab0f6adda1a9a7893
Max Ribbans <ribbans.max@gmail.com>
2021-05-08 12:15:54 -0400

    Move current user identity puts to channel;

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,16 +1,4 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    identified_by :current_user
-
-    def connect
-      self.current_user = find_verified_user
-    end
-
-    protected
-    def find_verified_user
-      puts "ENCRYPTED COOKIES: #{cookies.encrypted}"
-      puts "user id? #{cookies.encrypted[:user_id]}"
-      puts "params? #{params[:token]}"
-    end
   end
 end
--- app/channels/friendly_games_channel.rb
+++ app/channels/friendly_games_channel.rb
@@ -1,5 +1,6 @@
 class FriendlyGamesChannel < ApplicationCable::Channel
   def subscribed
+    find_verified_user(params)
     GamesFacade.add_player?(params[:extension], current_user.id) if :authenticate_user
     stream_from "friendly_games_channel_#{params[:extension]}"
     ActionCable.server.broadcast "friendly_games_channel_#{params[:extension]}", FriendlyGameSerializer.new(FriendlyGame.find_by(extension: params[:extension]))
@@ -7,5 +8,13 @@ class FriendlyGamesChannel < ApplicationCable::Channel
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  protected
+
+  def find_verified_user(params)
+    puts "ENCRYPTED COOKIES: #{cookies.encrypted}"
+    puts "user id? #{cookies.encrypted[:user_id]}"
+    puts "params? #{params[:token]}"
   end
 end
