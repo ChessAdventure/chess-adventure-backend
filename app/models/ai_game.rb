ed1fd5236c62f7e3da3e8e82160ff987fbfa8d82
Max Ribbans <ribbans.max@gmail.com>
2021-05-09 12:52:13 -0400

    Add enums to ai

--- app/models/ai_game.rb
+++ app/models/ai_game.rb
@@ -1,3 +1,14 @@
 class AiGame < ApplicationRecord
   belongs_to :ai_quest
+  after_initialize :default_fen, :default_status
+
+  enum status: [:in_progress, :won, :lost, :drawn]
+
+  def default_fen
+    self.current_fen ||= self.starting_fen
+  end
+
+  def default_status
+    self.status ||= 0
+  end
 end
--- app/models/ai_quest.rb
+++ app/models/ai_quest.rb
@@ -1,3 +1,11 @@
 class AiQuest < ApplicationRecord
   belongs_to :black_id
+
+  after_initialize :default_status
+
+  enum status: [:in_progress, :ended]
+
+  def default_status
+    self.status ||= 0
+  end
 end
